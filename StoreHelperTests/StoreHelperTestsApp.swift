c4373c3e217492243b1eb11b070800819424d63b
Russell Archer <russell.archer@mac.com>
2023-01-30 13:05:57 +0000

    Initial Commit

---
+++ StoreHelperTests/StoreHelperTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoreHelperTestsApp.swift
+//  StoreHelperTests
+//
+//  Created by Russell Archer on 30/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct StoreHelperTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
